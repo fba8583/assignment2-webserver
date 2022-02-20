550af17a29d36a087f807579841548ae23df3e00
fba8583 <98762381+fba8583@users.noreply.github.com>
2022-02-20 05:31:21 +0400

    Spree10

--- solution.py
+++ solution.py
@@ -34,33 +34,35 @@ def webServer(port=13331):
 
             # Send one HTTP header line into socket.
             # Fill in start
-
+            connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
             # Fill in end
 
             # Send the content of the requested file to the client
-            # for i in range(0, len(outputdata)):
-            # connectionSocket.send(outputdata[i].encode())
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
 
-            # connectionSocket.send("\r\n".encode())
-            # connectionSocket.close()
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
         except IOError:
             # Send response message for file not found (404)
             # Fill in start
+            connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
 
             # Fill in end
 
             # Close client socket
             # Fill in start
-
+            connectionSocket.close()
             # Fill in end
 
             # except (ConnectionResetError, BrokenPipeError):
             # pass
 
-            serverSocket.close()
+        serverSocket.close()
 
 
-# sys.exit()  # Terminate the program after sending the corresponding data
+sys.exit()  # Terminate the program after sending the corresponding data
+
 
 def main():
     webServer(13331)
